83a124422745671f6f731bb589fd37cbe2632570
researchersec <109282836+researchersec@users.noreply.github.com>
2024-10-11 07:01:03 +0200

    Update db.rs

--- src/db.rs
+++ src/db.rs
@@ -4,6 +4,7 @@ use rustls_pemfile::{certs, pkcs8_private_keys};
 use std::fs::File;
 use std::io::BufReader;
 use webpki::TrustAnchor;
+use webpki_roots::TLS_SERVER_ROOTS;
 use tokio_postgres_rustls::MakeRustlsConnect;
 use deadpool_postgres::{Config, Pool};
 use std::error::Error;
@@ -12,11 +13,11 @@ pub fn create_pool() -> Result<Pool, Box<dyn Error>> {
     // Load root certificates
     let mut root_cert_store = RootCertStore::empty();
     let trust_anchors: Vec<TrustAnchor> = TLS_SERVER_ROOTS.0.iter().copied().collect();
-    root_cert_store.add_server_trust_anchors(trust_anchors.into_iter().map(|anchor| {
+    root_cert_store.add_server_trust_anchors(trust_anchors.iter().map(|ta| {
         rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
-            anchor.subject,
-            anchor.spki,
-            anchor.name_constraints,
+            ta.subject,
+            ta.spki,
+            ta.name_constraints,
         )
     }));
 
@@ -34,7 +35,7 @@ pub fn create_pool() -> Result<Pool, Box<dyn Error>> {
     let tls_config = Arc::new(tls_config);
 
     // Create Rustls connector
-    let rustls_connector = MakeRustlsConnect::new(tls_config.clone());
+    let rustls_connector = MakeRustlsConnect::new((*tls_config).clone());
 
     // Configure Deadpool Postgres
     let mut pool_config = Config::new();
